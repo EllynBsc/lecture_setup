f84628aa99a2818ef886b9d28be35128d3a968ee
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2020-02-03 18:06:07 +0100

    lecture setup

---
+++ git_commands.rb
@@ -0,0 +1,18 @@
+*****************************************************************************
+CHEATSHEET GIT & GITHUB
+******************************************************************************
+# 1.Check your status
+git status
+
+# 2.Select the files to be committed
+git add file_1_which_has_been_modified_or_created
+git add file_2_which_has_been_modified_or_created
+# or select all the files in current directory
+git add .  (shortcut: ga .)
+
+# 3. Commit your work with a meaningfull message
+git commit -m "Why I did this change" (shortcut: gc -m "message")
+
+# 4.Push your exercise to kitt/github with good style
+git push origin master
+# => Make sure your git status is clean before you go on a new exercise or you leave your computer at night
---
+++ terminal_commands.rb
@@ -0,0 +1,37 @@
+******************************************************************************
+CHEATSHEET TERMINAL
+******************************************************************************
+command prompt/command line/console/terminal
+
+
+# print working directory
+pwd
+# list files(short list)
+ls
+# list files (long list) with more infos(permissions, dates, author etc..)
+ll
+# list the hidden files (always start with a dot)
+ls -a
+#Travelling in your computer: change directory/go to specific folder
+cd name_folder
+# up one folder -> parent directory
+cd ..
+# back to home/root folder
+cd
+#Create a directory
+mkdir name_directory
+#Create a file
+touch file_name.rb
+#Remove a file
+rm name_file.r
+#Remove a folder
+rm -rf name_dir
+# Move a file/directory
+mv name_file.rb name_new_folder_path
+# Rename a file/directory
+mv old_name.rb new_name.rb
+# Open Sublime text
+stt
+subl (for ubuntus)
+# View content of a file
+cat name_file.rb
